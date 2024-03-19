252bfcce21bd57075da175efeea9b676f2197364
Ubuntu <ubuntu@ip-172-31-35-85.ap-south-1.compute.internal>
2024-03-19 11:31:30 +0000

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+IMAGE_NAME="my-react-app"
+DOCKER_USERNAME="suryaa05"
+docker login -u $DOCKER_USERNAME -p dckr_pat_NZiazd1UpDiiMVDwt0dExnj8TcA
+
+build_and_push_image() {
+    local repo=$1
+    docker build -t $DOCKER_USERNAME/$repo .
+    docker push $DOCKER_USERNAME/$repo
+    if [ $? -eq 0 ]; then
+        echo "Image $IMAGE_NAME pushed successfully to $repo repository on Docker Hub."
+    else
+        echo "Error: Failed to push image $IMAGE_NAME to $repo repository on Docker Hub."
+        exit 1
+    fi
+}
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <branch>"
+    exit 1
+fi
+
+BRANCH=$1
+
+if [ "$BRANCH" = "dev" ]; then
+    build_and_push_image "dev"
+elif [ "$BRANCH" = "master" ]; then
+    build_and_push_image "prod"
+else
+    echo "Error: Invalid branch. The supported branches are 'dev' and 'master'."
+    exit 1
+fi
+
