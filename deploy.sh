3826e47009df69df2fca0e4a361e7756e44fa03a
suryaaB <90106982+suryaaB@users.noreply.github.com>
2024-03-19 18:33:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,27 +5,17 @@ DOCKER_USERNAME="suryaa05"
 docker login -u $DOCKER_USERNAME -p dckr_pat_NZiazd1UpDiiMVDwt0dExnj8TcA
 
 build_and_push_image() {
-    local repo=$1
-    docker build -t $DOCKER_USERNAME/$repo .
-    docker push $DOCKER_USERNAME/$repo
-    if [ $? -eq 0 ]; then
-        echo "Image $IMAGE_NAME pushed successfully to $repo repository on Docker Hub."
-    else
-        echo "Error: Failed to push image $IMAGE_NAME to $repo repository on Docker Hub."
-        exit 1
-    fi
+    docker build -t $DOCKER_USERNAME/prod .
+    docker push $DOCKER_USERNAME/prod
+}
+build_and_push_image() {
+    docker build -t $DOCKER_USERNAME/dev .
+    docker push $DOCKER_USERNAME/dev 
 }
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <branch>"
-    exit 1
-fi
-
-BRANCH=$1
-
-if [ "$BRANCH" = "dev" ]; then
+if [ "$GIT_BRANCH" = "orgin/dev" ]; then
     build_and_push_image "dev"
-elif [ "$BRANCH" = "master" ]; then
+elif [ "$GIT_BRANCH" = "origin/master" ]; then
     build_and_push_image "prod"
 else
     echo "Error: Invalid branch. The supported branches are 'dev' and 'master'."
